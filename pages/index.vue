d762b3334084b2ceb43419d88f5d125a5e7305f1
shoshi <shoshiroberts@gmail.com>
2018-10-11 14:18:01 -0400

    Styling plan section

--- pages/index.vue
+++ pages/index.vue
@@ -43,42 +43,92 @@
       </div> <!-- .wrapper -->
     </section>
 
-    <section id="plan" class="sml-pad-y3 med-pad-y6 fill-grey-dark">
+    <section id="plan" class="sml-pad-y3 med-pad-y6">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>The Plan</h2>
+            <h2 class="section-title">Here&rsquo;s the Plan</h2>
+            <p class="sml-push-y2 med-push-y3">
+              The FCC killed net neutrality, giving companies like Comcast and
+              Verizon the power to control what we see and do online with
+              throttling, censorship, and expensive new fees. But Congress can
+              still reverse that decision using the Congressional Review Act
+              (CRA). The Senate already passed a resolution, but now the House
+              is running out of time to do the same. We&rsquo;re focusing all of
+              our energy on key lawmakers who are facing tight races, making
+              them the most likely to listen to constituents. Here&rsquo;s what
+              we&rsquo;re raising money to do:
+            </p>
+            <div class="fill-grey-dark with-border is-rounded sml-push-y2 med-push-y3 sml-pad-2 med-pad-3">
+              <h4 class="flex-center">
+                <img src="~/assets/images/gradient-calls.png" alt="phone"
+                     class="gradient-icon" />
+                <span class="sml-pad-x2">
+                  Flood Congress with phone calls, tweets, and emails
+                </span>
+              </h4>
+              <p class="sml-push-y2">
+                When millions of people call and email their representatives,
+                Congress listens. Your donations will help us direct pressure in
+                key districts where it matters most.
+              </p>
+            </div> <!-- fill -->
+            <div class="fill-grey-dark with-border is-rounded sml-push-y2 sml-pad-2 med-pad-3">
+              <h4 class="flex-center">
+                <!-- TODO: replace icon -->
+                <img src="~/assets/images/gradient-calls.png" alt="phone"
+                     class="gradient-icon" />
+                <span class="sml-pad-x2">
+                  Run crowdfunded billboards, newspaper ads, and online ads
+                </span>
+              </h4>
+              <p class="sml-push-y2">
+                As politicians in key districts are hearing from outraged voters,
+                we&rsquo;ll turn up the pressure by putting up billboards and
+                running ads in digital and print media.
+              </p>
+            </div> <!-- fill -->
+            <div class="fill-grey-dark with-border is-rounded sml-push-y2 sml-pad-2 med-pad-3">
+              <h4 class="flex-center">
+                <!-- TODO: replace icon -->
+                <img src="~/assets/images/gradient-calls.png" alt="phone"
+                     class="gradient-icon" />
+                <span class="sml-pad-x2">
+                  Organize in-district actions and events
+                </span>
+              </h4>
+            </div> <!-- fill -->
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
 
-    <section id="defenders" class="sml-pad-y3 med-pad-y6">
+    <section id="defenders" class="sml-pad-y3 med-pad-y6 fill-grey-dark">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>The Defenders</h2>
+            <h2 class="section-title">The Defenders</h2>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
 
 
-    <section id="join" class="sml-pad-y3 med-pad-y6 fill-grey-dark">
+    <section id="join" class="sml-pad-y3 med-pad-y6">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>Join Us</h2>
+            <h2 class="section-title">Join Us</h2>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
 
-    <section id="donate" class="sml-pad-y3 med-pad-y6">
+    <section id="donate" class="sml-pad-y3 med-pad-y6 fill-grey-dark">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>Donate</h2>
+            <h2 class="section-title">Donate</h2>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
