45165807192c47eb01953ef679aca8f1ccab365d
shoshi <shoshiroberts@gmail.com>
2018-10-12 09:23:34 -0400

    Add sharing copy

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A Fight for the Future website' }
+      { hid: 'description', name: 'description', content: 'This is our last best chance to save net neutrality' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
