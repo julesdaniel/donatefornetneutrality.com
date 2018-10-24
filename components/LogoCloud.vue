24299cc69af0812bdffb35000c46b2ac5925659d
shoshi <shoshiroberts@gmail.com>
2018-10-24 16:08:54 -0400

    Add new internet champions

--- components/LogoCloud.vue
+++ components/LogoCloud.vue
@@ -3,39 +3,55 @@
     <div class="row">
       <div class="sml-c12 med-c4">
         <div class="fill-white is-rounded sml-pad-1">
-          <img src="~assets/images/logos/private-internet-access.png"
+          <img src="~assets/images/logos/cory-doctorow.png"
                class="grid-center"
-               alt="Private Internet Access"/>
+               alt="Cory Doctorow"/>
         </div> <!-- .fill -->
       </div> <!-- .c -->
       <div class="sml-c12 med-c4">
         <div class="fill-white is-rounded sml-pad-1 sml-push-y1 med-push-y0">
-          <img src="~assets/images/logos/startpage.png"
+          <img src="~assets/images/logos/evangeline-lilly.png"
                class="grid-center"
-               alt="StartPage"/>
+               alt="Evangeline Lilly"/>
         </div> <!-- .fill -->
       </div> <!-- .c -->
       <div class="sml-c12 med-c4 sml-push-y1 med-push-y0">
         <div class="fill-white is-rounded sml-pad-1">
-          <img src="~assets/images/logos/evangeline-lilly.png"
+          <img src="~assets/images/logos/tom-morello.png"
                class="grid-center"
-               alt="Evangeline Lilly"/>
+               alt="Tom Morello"/>
         </div> <!-- .fill -->
       </div> <!-- .c -->
     </div> <!-- .row -->
     <div class="row">
       <div class="sml-c12 med-c4 sml-push-y1 med-push-y3">
         <div class="fill-white is-rounded sml-pad-1">
-          <img src="~assets/images/logos/cory-doctorow.png"
+          <img src="~assets/images/logos/ifixit.png"
                class="grid-center"
-               alt="Cory Doctorow"/>
+               alt="ifixit"/>
         </div> <!-- .fill -->
       </div> <!-- .c -->
       <div class="sml-c12 med-c4 sml-push-y1 med-push-y3">
         <div class="fill-white is-rounded sml-pad-1">
-          <img src="~assets/images/logos/tom-morello.png"
+          <img src="~assets/images/logos/postmates.png"
                class="grid-center"
-               alt="Tom Morello"/>
+               alt="Postmates"/>
+        </div> <!-- .fill -->
+      </div> <!-- .c -->
+      <div class="sml-c12 med-c4 sml-push-y1 med-push-y3">
+        <div class="fill-white is-rounded sml-pad-1">
+          <img src="~assets/images/logos/private-internet-access.png"
+               class="grid-center"
+               alt="Private Internet Access"/>
+        </div> <!-- .fill -->
+      </div> <!-- .c -->
+    </div> <!-- .row -->
+    <div class="row">
+      <div class="sml-c12 med-c4 sml-push-y1 med-push-y3 sml-s0 med-s4">
+        <div class="fill-white is-rounded sml-pad-1">
+          <img src="~assets/images/logos/startpage.png"
+               class="grid-center"
+               alt="StartPage"/>
         </div> <!-- .fill -->
       </div> <!-- .c -->
     </div> <!-- .row -->
