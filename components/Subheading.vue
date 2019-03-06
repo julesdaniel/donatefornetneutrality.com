f484cbca993648695cb2fedea00ea3ab6fb5b636
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2019-03-06 11:30:59 -0500

    Update Subheading.vue

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -1,9 +1,13 @@
  <!-- NOTE: This is only in a component for A/B Testing, move to index.vue when test is over -->
 <template>
   <p class="sml-push-y2 med-push-y3">
-    Time is running out to get Congress to overrule the FCC and
-    restore net neutrality. We have a plan to turn up the pressure
-    on key lawmakers, and with the clock ticking a team of Internet
-    defenders are prepared to match your donation to make it possible.
+    Congress just 
+    <a href="https://www.reuters.com/article/us-usa-internet/democrats-to-push-to-reinstate-repealed-net-neutrality-rules-idUSKCN1QL1W0" 
+    target="_blank">introduced</a> the “Save the Internet Act” to 
+    overturn the FCC’s net neutrality repeal. Telecom lobbyists are 
+    gearing up to swarm Capitol Hill to stop the bill, so we need to 
+    push lawmakers extra hard to pass it.  Your donations will be 
+    matched by our Internet champions to ensure that Congress knows
+    how important Net Neutrality is to the American people.
   </p>
 </template>
