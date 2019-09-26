c1bbc2f94f1cd0ed51112d266df23bcf2bdfe2df
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2019-09-26 11:57:33 -0400

    Update Subheading.vue

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -1,13 +1,11 @@
  <!-- NOTE: This is only in a component for A/B Testing, move to index.vue when test is over -->
 <template>
   <p class="sml-push-y2 med-push-y3">
-    Congress just 
-    <a href="https://www.reuters.com/article/us-usa-internet/democrats-to-push-to-reinstate-repealed-net-neutrality-rules-idUSKCN1QL1W0" 
-    target="_blank">introduced</a> the “Save the Internet Act” to 
-    overturn the FCC’s net neutrality repeal. Telecom lobbyists are 
-    gearing up to swarm Capitol Hill to stop the bill, so we need to 
-    push lawmakers extra hard to pass it.  Your donations will be 
-    matched by our Internet champions to ensure that Congress knows
-    how important Net Neutrality is to the American people.
+    Since Ajit Pai's FCC gutted net neutrality rules, ISPs have been throttling Internet speeds
+    "<a href="https://www.bloomberg.com/news/articles/2019-08-19/wireless-carrier-throttling-of-online-video-is-pervasive-study" 
+    target="_blank">all the time</a>." But an 
+    <a href="https://gizmodo.com/fcc-faces-off-in-net-neutrality-lawsuit-against-consume-1832269448" 
+    target="_blank">important lawsuit</a> could force the FCC to reinstate net neutrality protections.
+    <b>Donate now and our Internet champions will match every dollar.</b>
   </p>
 </template>
