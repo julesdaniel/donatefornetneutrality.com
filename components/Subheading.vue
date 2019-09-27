742f3ade1c30895a3cf15c46251a27c63c7d8146
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2019-09-27 15:18:05 -0400

    Update Subheading.vue

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -4,9 +4,7 @@
     Since Ajit Pai's FCC gutted net neutrality rules, ISPs have been throttling Internet speeds for
     users everywhere. But an important lawsuit could force the FCC to reinstate net neutrality 
     protections. The ruling will be delievered any day, and we need to be prepared to fight big cable
-    companies regardless of the outcome.
-  </p>
-  <p>
-    <strong>Donate now and our Internet champions will match every dollar.</strong>
+    companies regardless of the outcome. <br><br><strong>Donate now and our Internet champions will 
+    match every dollar.</strong>
   </p>
 </template>
