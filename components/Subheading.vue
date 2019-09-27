74faf8e3aaa65624fcee5da99cf04fe58e582550
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2019-09-27 14:53:23 -0400

    Update Subheading.vue

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -1,11 +1,9 @@
  <!-- NOTE: This is only in a component for A/B Testing, move to index.vue when test is over -->
 <template>
   <p class="sml-push-y2 med-push-y3">
-    Since Ajit Pai's FCC gutted net neutrality rules, ISPs have been throttling Internet speeds
-    "<a href="https://www.bloomberg.com/news/articles/2019-08-19/wireless-carrier-throttling-of-online-video-is-pervasive-study" 
-    target="_blank">all the time</a>." But an 
-    <a href="https://gizmodo.com/fcc-faces-off-in-net-neutrality-lawsuit-against-consume-1832269448" 
-    target="_blank">important lawsuit</a> could force the FCC to reinstate net neutrality protections.
-    <b>Donate now and our Internet champions will match every dollar.</b>
+    Since Ajit Pai's FCC gutted net neutrality rules, ISPs have been throttling Internet speeds for
+    users everywhere. But an important lawsuit could force the FCC to reinstate net neutrality 
+    protections. The ruling will be delievered any day, and we need to be prepared for action 
+    whatever the outcome. <strong>Donate now and our Internet champions will match every dollar.</strong>
   </p>
 </template>
