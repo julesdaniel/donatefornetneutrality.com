6818597452f3122c375a3ac5f43af8476d7a5c73
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2019-10-01 12:35:43 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
               A bombshell court decision has shifted the fight for net neutrality to the state level.  Lobbyists
               for big cable companies are already swarming Congress in an effort to introduce bad legislation that
               will prevent states like California, Georgia, and Kentucky from enacting their own Internet protections.
-              But we're fighting back. <br><br><strong>Donate now and our Internet champions will match every dollar.</strong>
+              But we're fighting back. <strong>Donate now and our Internet champions will match every dollar.</strong>
             </p>
             <ProgressBar
               :current-total="currentAmountDonated"
